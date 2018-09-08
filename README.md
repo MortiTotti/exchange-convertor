ab88e17a3bd757ede032652fec4fd0f393b89e24
Morteza Taghdisi <morteza.taghdisi@yahoo.com>
2018-09-08 15:05:20 +0430

    initialize

---
+++ README.md
@@ -0,0 +1,2 @@
+## Table of Contents
+Simple React Native app for converting the currencies to each other
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export YasContainer from './Yas-Container';
+export YasContent from './Yas-Content';
+export YasText from './Yas-Text';
+export YasTextInput from './Yas-Text-Input';
+export YasNavbarTitle from './Yas-Navbar-Title';
